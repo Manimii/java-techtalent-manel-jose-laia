861dd4e0f7bb10e606fdd31670097b47a69faa30
Manel Nin <mninminet@gmail.com>
2024-03-15 10:30:06 +0100

    Update MainApp.java

--- UD08Supermercado/src/UD08/MainApp.java
+++ UD08Supermercado/src/UD08/MainApp.java
@@ -3,23 +3,9 @@ package UD08;
 public class MainApp {
 	
 	public static void main(String[] args) {
-		holaManel();
-		HolaJose();
-		Holamundo();
+		
 
 	}
 
-	public static void holaManel() {
-        System.out.println("Hola soy manel");
-        
-    }
 	
-    public static void HolaJose (){
-        System.out.println("Hola Jose");
-    }
-    
-    public static void Holamundo(){
-        System.out.println ("Hola soy Laia");
-
-    }
 }
